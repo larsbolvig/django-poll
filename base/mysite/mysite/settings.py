36553b0cedf6e01b0fb8cd5879b57f6e4a958a76
Lars Bolvig <larsbolvig@gmail.com>
2013-12-23 17:56:05 +0100

    Make the app use postgres instead of sqlite3

--- base/mysite/mysite/settings.py
+++ base/mysite/mysite/settings.py
@@ -55,10 +55,27 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project_dev',  # Or path to database file if using sqlite3.
+
+        # The following settings are not used with sqlite3:
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+
+        # Empty for localhost through domain sockets or
+        # '127.0.0.1' for localhost through TCP.
+        'HOST': '40.40.40.40',
+        'PORT': '5432',  # Set to empty string for default.
     }
 }
 
